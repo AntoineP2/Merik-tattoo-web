f21b1b4b71b7cbd1e7915d8ce6372efeafceeccf
AntoineP2 <antoine.vinsonneau34@gmail.com>
2024-09-11 16:18:31 +0200

    Email change

--- app/api/mail/custom/route.ts
+++ app/api/mail/custom/route.ts
@@ -1,14 +1,14 @@
 import Email from "@/emails/userMailFlash";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY_OFFICIAL);
 
 export async function POST(req: Request, res: Response) {
-  const { name, surname } = await req.json();
+  const { name, surname, email } = await req.json();
 
   const { data, error } = await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "antoine.vinsonneau34@gmail.com",
+    from: "MerikTattoo Information <contact@padovaniwebdevelopment.fr>",
+    to: email,
     subject: `Validation formulaire ${name}`,
     react: Email({
       name,
--- app/api/mail/flash/recap/route.ts
+++ app/api/mail/flash/recap/route.ts
@@ -1,15 +1,15 @@
 import Email from "@/emails/recapMailFlash";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY_OFFICIAL);
 
 export async function POST(req: Request, res: Response) {
   const { name, surname, email, phone, reference, placement, disponibility, dayOfWeek } =
     await req.json();
 
   const { data, error } = await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "antoine.vinsonneau34@gmail.com", // A changer en mail quand on auras le nom de domaine
+    from: "MerikTattoo Information <contact@padovaniwebdevelopment.fr>",
+    to: email, // A changer en mail quand on auras le nom de domaine
     subject: `Validation formulaire ${name}`,
     react: Email({
       name,
